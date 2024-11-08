0eb3521f8917c7dbd7c6f52a5761d83e649a49cc
Benighter <111303968+Benighter@users.noreply.github.com>
2024-11-08 16:02:38 +0200

    first commit

---
+++ server/INSERT INTO tasks (description, date).sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks (description, date)
+VALUES (
+    'Hello',
+    '2024-03-29'
+  );
---
+++ todo_app.session.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE tasks(
+--     task_id SERIAL PRIMARY KEY ,
+--     description VARCHAR(30) NOT NULL ,
+--     date DATE
+-- );
+
+SELECT * FROM tasks;
+
+-- INSERT INTO tasks (description, date) VALUES ('Task 1', '2022-01-01');
